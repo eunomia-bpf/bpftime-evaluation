509d82096e5c97805b9d9e4717e7ac8810c0477f
officeyutong <yt.xyxx@gmail.com>
2023-10-20 15:00:54 +0800

    Update with nginx tests

--- sslsniff/test_wget_bpftime.sh
+++ sslsniff/test_wget_bpftime.sh
@@ -3,5 +3,5 @@ sleep 3s
 bg_id=$!
 nginx -c $(pwd)/nginx.conf -p $(pwd)
 time bpftime start -- wget https://127.0.0.1:4043 -O /dev/null --no-check-certificate --no-proxy
-kill -SIGKILL $!
+kill -SIGKILL $bg_id
 nginx -c $(pwd)/nginx.conf -s stop
--- sslsniff/test_wget_kernel.sh
+++ sslsniff/test_wget_kernel.sh
@@ -2,5 +2,5 @@
 bg_id=$!
 nginx -c $(pwd)/nginx.conf -p $(pwd)
 time wget https://127.0.0.1:4043 -O /dev/null --no-check-certificate --no-proxy
-kill -SIGKILL $!
+kill -SIGKILL $bg_id
 nginx -c $(pwd)/nginx.conf -s stop
