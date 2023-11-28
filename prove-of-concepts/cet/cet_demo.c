9c72c4785de8e57963b5b93be4c5a473db9596d9
victoryang00 <yangyiwei2000@gmail.com>
2023-11-28 04:55:09 +0000

    fix new rocksdb code

--- prove-of-concepts/cet/cet_demo.c
+++ prove-of-concepts/cet/cet_demo.c
@@ -1,48 +1,48 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-
-// void safe_function() {
-//     printf("Safe function called.\n");
-// }
-
-// void unsafe_function() {
-//     printf("Unsafe function called.\n");
-// }
-
-// int main() {
-//     // 正常的函数指针使用
-//     void (*func_ptr)() = safe_function;
-//     func_ptr();
-
-//     // 尝试非法修改函数指针
-//     // 在一个CET支持的环境中，这种修改会被检测并阻止
-//     func_ptr = (void (*)())((char*)unsafe_function+4);
-//     printf("func_ptr = %p, original ptr = %p\n", func_ptr, unsafe_function);
-//     func_ptr(); // 这里的调用在CET环境下应该会失败
-
-//     return 0;
-// }
-
-
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
-void func1(void) { printf("[+] Success: Inside func1\n"); }
-void func2(void) { printf("[+] Success: Inside func2\n"); }
-int main(void)
-{
-  void (*fptr)(void);
-  pid_t (*getpid_fptr)(void);
-  pid_t pid;
-  printf("[o] Indirectly calling external function: getpid()\n");
-  getpid_fptr = &getpid;
-  pid = getpid_fptr();
-  printf("[+] Success: PID = %jd\n", (intmax_t)pid);
-  printf("[o] Directly calling function: func1()\n");
-  func1();
-  printf("[o] Indirectly calling function: func2()\n");
-  func2();
-  printf("[+] Done.\n");
+
+void safe_function() {
+    printf("Safe function called.\n");
 }
+
+void unsafe_function() {
+    printf("Unsafe function called.\n");
+}
+
+int main() {
+    // 正常的函数指针使用
+    void (*func_ptr)() = safe_function;
+    func_ptr();
+
+    // 尝试非法修改函数指针
+    // 在一个CET支持的环境中，这种修改会被检测并阻止
+    func_ptr = (void (*)())((char*)unsafe_function);
+    printf("func_ptr = %p, original ptr = %p\n", func_ptr, unsafe_function);
+    func_ptr(); // 这里的调用在CET环境下应该会失败
+
+    return 0;
+}
+
+
+// #include <stdint.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <sys/types.h>
+// #include <unistd.h>
+// void func1(void) { printf("[+] Success: Inside func1\n"); }
+// void func2(void) { printf("[+] Success: Inside func2\n"); }
+// int main(void)
+// {
+//   void (*fptr)(void);
+//   pid_t (*getpid_fptr)(void);
+//   pid_t pid;
+//   printf("[o] Indirectly calling external function: getpid()\n");
+//   getpid_fptr = &getpid;
+//   pid = getpid_fptr();
+//   printf("[+] Success: PID = %jd\n", (intmax_t)pid);
+//   printf("[o] Directly calling function: func1()\n");
+//   func1();
+//   printf("[o] Indirectly calling function: func2()\n");
+//   func2();
+//   printf("[+] Done.\n");
+// }
--- rockdb-bpf/bpf/rocksdb.bpf.c
+++ rockdb-bpf/bpf/rocksdb.bpf.c
@@ -8,11 +8,63 @@
 #include <bpf/bpf_tracing.h>
 #include "rocksdb.h"
 
-SEC("tracepoint/syscalls/sys_enter_statfs")
-int tracepoint__syscalls__sys_enter_statfs(struct trace_event_raw_sys_enter *ctx)
+struct uring_check_id{
+    u64 jobid;
+    u32 inode;
+    u32 hashed_inode;
+};
+
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __type(key, u64); // pid
+    __type(value, struct uring_check_id); // bitmap get murmurhash
+    __uint(max_entries, 1000);
+    __uint(map_flags, BPF_F_NO_PREALLOC);
+} bpftime_uring_map SEC(".maps");
+
+//SEC("uretprobe/rocksdb:_ZN7rocksdb6Urings14wait_for_queueEPNS_11uring_queueE")
+//int wait_f(_ZN7rocksdb6Urings14wait_for_queueEPNS_11uring_queueE, int a, int b)
+//{
+//    bpf_printk("uprobed_sub ENTRY: a = %d, b = %d", a, b);
+//    return 0;
+//}
+
+SEC("uprobe/rocksdb:io_uring_wait_cqe")
+int BPF_UPROBE(io_uring_wait_cqe)
 {
-	// bpf_printk("trace_enter sys_enter_statfs\n");
-	return 0;
+    // if it's ready, set the iouring done flag and continue for this fd
+    bpf_printk("io_uring_wait_cqe");
+    return 0;
+}
+
+SEC("uprobe/rocksdb:io_uring_get_sqe")
+int BPF_UPROBE(io_uring_get_sqe)
+{ // get fd submit id pid
+    // insert job id pid to map
+    bpf_printk("io_uring_get_sqe");
+    return 0;
+}
+
+// 找到一个kernel
+// trace点可以把 fd 的 inodemap拿到
+//SEC("kretprobe:__writeback_single_inode")
+//int check_done_ext_writeback(struct pt_regs *ctx)
+//{
+//    u32 pid = bpf_get_current_pid_tgid();
+//    u64 *val, key = 0;
+//    val = bpf_map_lookup_elem(&bpftime_uring_map, &key);
+//    bpf_printk("__writeback_single_inode ENTRY: a = %d, b = %d", key, pid);
+//
+//    return 0;
+//}
+
+SEC("kretprobe:__writeback_single_inode")
+int BPF_KRETPROBE(__writeback_single_inode)
+{
+    u32 pid = bpf_get_current_pid_tgid();
+    bpf_printk("__writeback_single_inode");
+
+    return 0;
 }
 
 char LICENSE[] SEC("license") = "GPL";
