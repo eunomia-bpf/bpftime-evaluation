1744feeb2e7d90f3c9909441c7275140685a2770
yunwei37 <yunwei356@gmail.com>
2023-11-30 06:49:32 +0000

    add code

--- prove-of-concepts/gate/enable.c
+++ prove-of-concepts/gate/enable.c
@@ -1,3 +1,14 @@
-int main(){
-    arch_prctl()
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/syscall.h>  
+
+int main() {
+  uint64_t *addr = malloc(sizeof(uint64_t) * 4);
+  arch_prctl(0x3001, addr);
+  printf("[+] Success: addr = %p\n", addr[0]);
+  printf("[+] Success: addr = %p\n", addr[1]);
+  printf("[+] Success: addr = %p\n", addr[2]);
 }
